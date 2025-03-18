b08e7a9969abd103e58eaedee1bfeb45e4f405e4
WardahSajjad <i211240@nu.edu.pk>
2025-03-19 01:22:46 +0400

    one more service added

--- src/components/Features/featuresData.tsx
+++ src/components/Features/featuresData.tsx
@@ -1,5 +1,5 @@
 import { Feature } from "@/types/feature";
-import { Book, FileText, Zap } from "lucide-react"; // Importing icons from lucide-react
+import { Book, FileText, Zap , UserRoundSearch} from "lucide-react"; // Importing icons from lucide-react
 
 const featuresData: Feature[] = [
   {
@@ -26,6 +26,14 @@ const featuresData: Feature[] = [
     paragraph:
       "Stay compliant with UK tax laws through expert bookkeeping and accounting services. We offer tailored solutions for VAT, Corporation Tax, Income Tax, and PAYE, ensuring accurate filing and timely submissions.",
     link: "/taxes",
+  },
+  {
+    id: 4,
+    icon: <UserRoundSearch size={40} color="#fa8705" />, // Using a FileText icon for taxes
+    title: "Outsourced Professional Accountants",
+    paragraph:
+      "Hire highly skilled, professional accountants who deliver the same (or better) expertise as in-house staff—at just 60-70% of the cost. No payroll taxes, insurance, or pensions—just one fixed monthly cost, and we handle everything else, from compliance to payroll. Scale your team effortlessly while keeping your bottom line strong.",
+    link: "/",
   },
   // Add more features as needed
 ];
